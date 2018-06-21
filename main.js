c523ebede947fbf5e535d1bf9075f973e89ee7aa
Justin Pallo <justinpallo@gmail.com>
2018-06-21 12:51:09 -0300

    Please enter the commit message for your changes. Lines starting Express web server set up

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-var http = require("http");
-http.createServer(function(request, response){
-    response.writeHead(200,{
-        'Content-Type': 'text/plain'
+import express from "express";
+var app = express();
+
+/*serves main page*/
+app.get("/", function(req, res){
+    res.sendfile('index.html')
+});
+
+    app.post("/user/add", function(req, res){
+        /*some server side logic*/
+        res.send("OK");
     });
-    response.write('Simple Simple Fun')
-    response.end();
-}).listen(5002);
+
+/*serves all the static files */
+app.get(/^(.+)$/, function(req, res){
+    console.log('static file request : ' + req.params);
+    res.sendfile(__dirname + req.params[0]);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
+    console.log("Listening on " + port);
+});
