f7aa3fb8967dad5a4da7f6803cb197957989cebd
Justin Pallo <justinpallo@gmail.com>
2018-06-21 13:57:39 -0300

    Setting up the files for the client

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var app = express();
 
 /*serves main page*/
 app.get("/", function(req, res){
-    res.sendfile('/pub/index.html')
+    res.sendFile(__dirname + '/pub/index.html')
 });
 
     app.post("/user/add", function(req, res){
@@ -14,7 +14,7 @@ app.get("/", function(req, res){
 /*serves all the static files */
 app.get(/^(.+)$/, function(req, res){
     console.log('static file request : ' + req.params);
-    res.sendfile(__dirname + req.params[0]);
+    res.sendFile(__dirname + "/pub/" + req.params[0]);
 });
 
 var port = process.env.PORT || 5000;
