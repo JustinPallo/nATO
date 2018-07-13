cef0a7a4957c47781a52eb27613dc973c95eecba
Justin Pallo <justinpallo@gmail.com>
2018-07-13 15:02:00 -0300

    Added .update functionality. click a list item and watch what happens!

--- pub/js/client.js
+++ pub/js/client.js
@@ -73,6 +73,8 @@ function buildApartments(aptData){
 
 //writes the array of Apartments to the page as new <li>s
 function updateList(apts){
+    var vw = new View;
+
     for(i = 0; i < apts.length; i++){
         $("#pensionSelect ul").append("<li id='" + apts[i].id + "'>" + apts[i].title() + "</li>");
     }
@@ -80,7 +82,7 @@ function updateList(apts){
     //sets up the enviroment so that when you click a button, stuff happens
     $("#pensionSelect ul li").click(function(event){
         var clickedID = $(event.target).text();
-        
+        vw.update(apts[searchArrayForName(apts, clickedID.substring(3))]);
     });
 }
 
@@ -96,6 +98,45 @@ function searchArrayForName(array, id){
     }
     //item not found
     return -1;
+}
+
+//the pink view to the right of the list
+class View{
+    constructor(){
+
+    }
+
+    update(apt){
+        $('h1.name').replaceWith("<h1 class='name'>" + apt.name + "</h1>");
+        $('h2.id').replaceWith("<h2 class'id'>" + apt.id + "</h2>");
+        $('h2.zone').replaceWith("<h2 class='zone'>" + apt.zone + "</h2>");
+        $('h2.address').replaceWith("<h2 class='address'>" + apt.address + "</h2>");
+        $('h3.state').replaceWith("<h3 class='state'>Estado: " + apt.state + "</h3>");
+        $('h3.inspectionDate').replaceWith("<h3 class='inspectionDate'>Fecha de Inspeción: " + apt.inspectionDate + "</h3>");
+        $('h3.paymentForm').replaceWith("<h3 class='paymentForm'>Forma de Pago: " + apt.paymentForm + "</h3>");
+        $('h3.provState').replaceWith("<h3 class='provState'>Estado de Proveedor: " + apt.provState + "</h3>");
+        $('h3.securityDeposit').replaceWith("<h3 class='securityDeposit'>Deposito de Seguridad: " + apt.securityDeposit + "</h3>");
+        $('h3.firstSemester').replaceWith("<h3 class='firstSemester'>Primer Semestre: " + apt.firstSemester + "</h3>");
+        $('h3.secondSemester').replaceWith("<h3 class='secondSemester'>Segundo Semestre: " + apt.secondSemester + "</h3>");
+        $('h3.thirdSemester').replaceWith(" <h3 class='thirdSemester'>Tercer Semestre: " + apt.thirdSemester + "</h3>");
+        $('h3.fourthSemester').replaceWith(" <h3 class='fourthSemester'>Cuarto Semestre: " + apt.fourthSemester + "</h3>");
+        $('h3.startDate').replaceWith("<h3 class='startDate'>Principio de Contrato: " + apt.startDate + "</h3>");
+        $('h3.endDate').replaceWith("<h3 class='endDate'>Fin de Contrato: " + apt.endDate + "</h3>");
+        $('h3.contractCopy').replaceWith("<h3 class='contractCopy'>Copia de Contrato: " + apt.contractCopy + "</h3>");
+        $('h3.numMissionaries').replaceWith("<h3 class='numMissionaries'>Numero de Misioneros: " + apt.numMissionaries + "</h3>");
+        $('h3.typeMissionaries').replaceWith("<h3 class='typeMissionaries'>Tipo de Misioneros: " + apt.typeMissionaries + "</h3>");
+        $('h3.llName').replaceWith("<h3 class='llName'>Inmobiliaria: " + apt.llName + "</h3>");
+        $('h3.llAddress').replaceWith("<h3 class='llAddress'>Dirección de Inmobiliaria: " + apt.llAddress + "</h3>");
+        $('h3.llPhone').replaceWith("<h3 class='llPhone'>Num. Telefonico de Inmobiliaria: " + apt.llPhone + "</h3>");
+        $('h3.llEmail').replaceWith("<h3 class='llEmail'>Correo de Inmobiliaria: " + apt.llEmail + "</h3>");
+        $('h3.oName').replaceWith("<h3 class='oName'>Nombre de Dueño: " + apt.oName + "</h3>");
+        $('h3.oCUIL').replaceWith("<h3 class='oCUIL'>CUIL de Dueño: " + apt.oCUIL + "</h3>");
+        $('h3.oPhone').replaceWith("<h3 class='oPhone'>Num. Telefonico de Dueño: " + apt.oPhone + "</h3>");
+        $('h3.oInfo').replaceWith("<h3 class='oInfo'>Info de Dueño: " + apt.oInfo + "</h3>");
+        $('h3.oEmail').replaceWith("<h3 class='oEmail'>Correo de Dueño: " + apt.oEmail + "</h3>");
+        $('h3.lightBox').replaceWith("<h3 class='lightBox'>Medidador de Luz: " + apt.lightBox + "</h3>");
+
+    }
 }
 
 //because I like classes to be organized
